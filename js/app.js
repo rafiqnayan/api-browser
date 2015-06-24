7202d58a4bd3ddeaf4c7b326c3c5827a5d714bf6
Rafiqunnabi Nayan <rafiqnayan@gmail.com>
2015-06-24 09:19:49 +0600

    Routing added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', [])
+var apiBrowserApp = angular.module('app', ['ngRoute','apiBrowserControllers'])
  
 .directive('apiList', function() {
     return {
@@ -7,11 +7,11 @@ angular.module('app', [])
       controller: function($scope, $element) {
         var panes = $scope.panes = [];
 
-        panes.push({'title':"Item 1", "selected":false});
-        panes.push({'title':"Item 2", "selected":false});
-        panes.push({'title':"Item 3", "selected":false});
-        panes.push({'title':"Item 4", "selected":false});
-        panes.push({'title':"Item 5", "selected":false});
+        panes.push({'title':"API 1", "selected":false, "link": "api-1"});
+        panes.push({'title':"API 2", "selected":false, "link": "api-2"});
+        panes.push({'title':"API 3", "selected":false, "link": "api-3"});
+        panes.push({'title':"API 4", "selected":false, "link": "api-4"});
+        panes.push({'title':"API 5", "selected":false, "link": "api-5"});
  
         $scope.select = function(pane) {
           angular.forEach(panes, function(pane) {
@@ -23,8 +23,20 @@ angular.module('app', [])
       template:
           '<ul class="nav nav-sidebar">' +
             '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
-              '<a href="" ng-click="select(pane)">{{pane.title}}</a>' +
+              '<a href="#/api/{{pane.link}}" ng-click="select(pane)">{{pane.title}}</a>' +
             '</li>' +
           '</ul>'
     };
   });
+
+apiBrowserApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/api/:apiId', {
+        templateUrl: 'api_details.html',
+        controller: 'ApiDetailController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ js/controllers.js
@@ -0,0 +1,7 @@
+var apiBrowserControllers = angular.module('apiBrowserControllers', []);
+
+apiBrowserControllers.controller('ApiDetailController', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.apiId = $routeParams.apiId;
+    console.log($scope.apiId);
+  }]);
