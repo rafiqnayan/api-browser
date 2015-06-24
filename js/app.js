77a0180c6ce8cf2806feb443ac39da4e0b477472
Rafiqunnabi Nayan <rafiqnayan@gmail.com>
2015-06-24 10:12:04 +0600

    Directives moved to separate file

--- js/app.js
+++ js/app.js
@@ -1,37 +1,5 @@
-var apiBrowserApp = angular.module('app', ['ngRoute','apiBrowserControllers'])
+var apiBrowserApp = angular.module('app', ['ngRoute','apiBrowserControllers', 'apiBrowserDirectives'])
  
-.directive('apiList', ['$location', '$http', function(location, $http) {
-    return {
-      scope: {},
-      replace: true,
-      controller: function($scope, $element) {
-        var panes = $scope.panes = [];
-        
-        $http.get('data/api_list.json').success(function(apiList){
-          apiList.forEach(function(entry){
-            if(location.path().split('/')[2] === entry.link){
-              entry.selected = true;
-            }
-            panes.push(entry);
-          });
-        });
-
-        $scope.select = function(pane) {
-          angular.forEach(panes, function(pane) {
-            pane.selected = false;
-          });
-          pane.selected = true;
-        }
-      },
-      template:
-          '<ul class="nav nav-sidebar">' +
-            '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
-              '<a href="#/api/{{pane.link}}" ng-click="select(pane)">{{pane.title}}</a>' +
-            '</li>' +
-          '</ul>'
-    };
-  }]);
-
 apiBrowserApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,4 @@ var apiBrowserControllers = angular.module('apiBrowserControllers', []);
 apiBrowserControllers.controller('ApiDetailController', ['$scope', '$routeParams',
   function($scope, $routeParams) {
     $scope.apiId = $routeParams.apiId;
-    // console.log($scope.panes);
   }]);
