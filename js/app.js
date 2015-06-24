98bb5d8650de25bd6863edcf4d73a9a9f33089a2
Rafiqunnabi Nayan <rafiqnayan@gmail.com>
2015-06-24 10:03:07 +0600

    API list loaded from json file

--- js/app.js
+++ js/app.js
@@ -1,25 +1,21 @@
 var apiBrowserApp = angular.module('app', ['ngRoute','apiBrowserControllers'])
  
-.directive('apiList', ['$location', function(location) {
+.directive('apiList', ['$location', '$http', function(location, $http) {
     return {
       scope: {},
       replace: true,
       controller: function($scope, $element) {
         var panes = $scope.panes = [];
-        var apiList = [
-          {'title':"API 1", "selected":false, "link": "api-1"},
-          {'title':"API 2", "selected":false, "link": "api-2"},
-          {'title':"API 3", "selected":false, "link": "api-3"},
-          {'title':"API 4", "selected":false, "link": "api-4"},
-          {'title':"API 5", "selected":false, "link": "api-5"}
-        ];
-        apiList.forEach(function(entry){
-          if(location.path().split('/')[2] === entry.link){
-            entry.selected = true;
-          }
-          panes.push(entry);
+        
+        $http.get('data/api_list.json').success(function(apiList){
+          apiList.forEach(function(entry){
+            if(location.path().split('/')[2] === entry.link){
+              entry.selected = true;
+            }
+            panes.push(entry);
+          });
         });
- 
+
         $scope.select = function(pane) {
           angular.forEach(panes, function(pane) {
             pane.selected = false;
