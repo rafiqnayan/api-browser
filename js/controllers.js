f4c23a5b508b5762c4855a83fe46f1ff56684230
Rafiqunnabi Nayan <nayan@revesoft.com>
2015-06-27 17:11:04 +0600

    UI modified

--- js/controllers.js
+++ js/controllers.js
@@ -5,6 +5,8 @@ apiBrowserControllers.controller('ApiDetailController', ['$scope', '$routeParams
     
     $http.get('/data/details/sample_api.json').success(function(data){
         $scope.data = data;
+
+        $scope.data.example.response = JSON.stringify(data.example.response, undefined, 2);
     });
 
   }]);
